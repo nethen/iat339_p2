92c7e5501d396f2fdc8d8468fae19d606acaa4db
nethen <72537484+nethen@users.noreply.github.com>
2023-06-12 16:22:35 -0700

    Added script for nav toggle, fixed font display

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+// All scripting is made by Nathan Lew based on prior knowledge and experimentation
+const toggleMenu = () => {
+    const menu = document.getElementsByClassName("nav__wrapper__items")[0];
+    menu.classList.toggle("nav__wrapper__items--active");
+}
