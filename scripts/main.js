2b2a8c6cf5eaaa7770b912f2c17ea81519a57ada
nethen <72537484+nethen@users.noreply.github.com>
2023-06-13 03:25:21 -0700

    Updated nav styling for grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 // All scripting is made by Nathan Lew based on prior knowledge and experimentation
 const toggleMenu = () => {
-    const menu = document.getElementsByClassName("nav__wrapper__items")[0];
-    const menuImgs = document.getElementsByClassName("nav__wrapper__menu__img");
+    const menu = document.getElementsByClassName("nav__overlay__wrapper")[0];
+    const menuImgs = document.getElementsByClassName("nav__overlay__menu__img");
     const body = document.getElementsByTagName("body")[0];
-    menu.classList.toggle("nav__wrapper__items--active");
+    menu.classList.toggle("nav__overlay__wrapper--active");
     for(let element of menuImgs) {
-        element.classList.toggle("nav__wrapper__menu__img--active");
+        element.classList.toggle("nav__overlay__menu__img--active");
     };
-    body.style.overflow = menu.classList.contains("nav__wrapper__items--active") ? "hidden" : "auto";
+    body.style.overflow = menu.classList.contains("nav__overlay__wrapper--active") ? "hidden" : "auto";
 }
 
 const closeMenu = () => {
-    const menu = document.getElementsByClassName("nav__wrapper__items")[0];
-    const menuImgs = document.getElementsByClassName("nav__wrapper__menu__img");
+    const menu = document.getElementsByClassName("nav__overlay__wrapper")[0];
+    const menuImgs = document.getElementsByClassName("nav__overlay__menu__img");
     const body = document.getElementsByTagName("body")[0];
-    menu.classList.remove("nav__wrapper__items--active");
-    menuImgs[0].classList.add("nav__wrapper__menu__img--active");
-    menuImgs[1].classList.remove("nav__wrapper__menu__img--active");
-    body.style.overflow = menu.classList.contains("nav__wrapper__items--active") ? "hidden" : "auto";
+    menu.classList.remove("nav__overlay__wrapper--active");
+    menuImgs[0].classList.add("nav__overlay__menu__img--active");
+    menuImgs[1].classList.remove("nav__overlay__menu__img--active");
+    body.style.overflow = menu.classList.contains("nav__overlay__wrapper--active") ? "hidden" : "auto";
 }
