928ccd874aef3b225aa131845e60666ef944fad1
nethen <72537484+nethen@users.noreply.github.com>
2023-07-07 16:41:26 -0700

    Fixed weird gap on menu

--- scripts/main.js
+++ scripts/main.js
@@ -61,22 +61,26 @@ const closeMenu = () => {
 //https://codepen.io/coreDeiv/pen/YzqzRKK
 //Handles where the images position is based on mouse position
 
+document.addEventListener("resize", function(){
+    let hoverable = (window.innerWidth >= 992);
+    if (hoverable) console.log("nice");
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     const menuItems = document.querySelectorAll(".menu-item-name");
     //checks for different image inputs for certain menu items
     menuItems.forEach(function(item) {
         const menuItemContainer = item.parentNode; // Get the parent container of the menu item
         const popupImage = menuItemContainer.querySelector(".popup-image"); // Get the respective popup image for the menu item
-
-        menuItemContainer.addEventListener("mousemove", function(event) {
-            popupImage.style.display = "block";
-            popupImage.style.left = event.clientX + "px";
-            popupImage.style.top = event.clientY + "px";
-        });
-
-        menuItemContainer.addEventListener("mouseleave", function() {
-            popupImage.style.display = "none";
-        });
+            menuItemContainer.addEventListener("mousemove", function(event) {
+                popupImage.style.display = "block";
+                popupImage.style.left = event.clientX + "px";
+                popupImage.style.top = event.clientY + "px";
+            });
+    
+            menuItemContainer.addEventListener("mouseleave", function() {
+                popupImage.style.display = "none";
+            });
     });
 });
 
@@ -84,74 +88,32 @@ document.addEventListener("DOMContentLoaded", function() {
 //Switch between sections to view food menu and drinks menu
 //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_state_switch1
 
-// Get references to the menu and sections
-const menu = document.getElementById('menu');
-if(menu)
-{
-    const sections = document.getElementsByClassName('section-menu');
-
-    menu.addEventListener('change', function() {
-        const selectedSection = menu.value;
-    
-        for (let i = 0; i < sections.length; i++) {
-            sections[i].classList.remove('active');
-        }
-    
-        const sectionToShow = document.getElementById(selectedSection);
-        if (sectionToShow) {
-            sectionToShow.classList.add('active');
-        }
-    });
-}
-
-
-
 //https://www.w3schools.com/jsref/prop_win_localstorage.asp
 // Get the menu select element and section menu elements
 const menuSelect = document.getElementById('menu');
-
-if (menuSelect){
-// Retrieve the active section from localStorage
-const activeSection = localStorage.getItem('activeSection');
-
-// Set the initial active section
-if (activeSection) {
-    const activeOption = menuSelect.querySelector(`option[value="${activeSection}"]`);
-    if (activeOption) {
-        activeOption.selected = true;
-    }
-}
 // Add event listener to the menu select element
 menuSelect.addEventListener('change', toggleSection);
 
 // Call the toggleSection function on page load
-toggleSection();
-}
-
-
-
-
+// toggleSection();
 
 //https://www.w3schools.com/js/js_api_web_storage.asp
 // Toggle the active section based on the selected option
 function toggleSection() {
-    const selectedOption = menuSelect.options[menuSelect.selectedIndex];
-    const selectedSectionId = selectedOption.value;
-
-    // Store the active section in localStorage
-    localStorage.setItem('activeSection', selectedSectionId);
-    const sectionMenus = document.querySelectorAll('.section-menu');
-
-    // Toggle the active class on section menus
-    sectionMenus.forEach((section) => {
-        section.classList.remove('active');
-    });
-
-    const selectedSection = document.getElementById(selectedSectionId);
-    if (selectedSection) {
-        selectedSection.classList.add('active');
+    const menu = document.getElementById('menu');
+    if(menu)
+    {
+        const sections = document.getElementsByClassName('menu-items');
+            const selectedSection = menu.value;
+            for (let i = 0; i < sections.length; i++){
+                if (sections[i].classList.contains(selectedSection)) sections[i].classList.remove("hidden");
+                else sections[i].classList.add("hidden");
+            }
     }
 }
+
+toggleSection();
+
 
 
 
